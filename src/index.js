47fe123c6c08cf4d6de1e04bb0836f75b6044db1
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-11-03 08:03:29 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+const serveStatic = require('serve-static');
+
+const app = express();
+
+// Parse JSON request bodies to JavaScript objects.
+app.use(bodyParser.json());
+
+// Logging
+// The provided options are combined, common, dev, short, and tiny.
+// For more details, browse https://github.com/expressjs/morgan.
+app.use(morgan('dev'));
+
+// Serve static files from the public directory.
+app.use(serveStatic('public'));
+
+const PORT = 3001;
+app.listen(PORT, () => console.log('listening on', PORT));
